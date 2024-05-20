121cfca33d9ef10595e2c4635e3980799832e180
Franco_Serjen <francoserjen100@gmail.com>
2024-05-20 12:13:41 -0300

    Creación de servicio para la conexión a la base de datos y se genero un DTO que esta en desarrollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginService } from './services/login/login.service';
+import { DatabaseService } from './common/db.service';
+import { JwtModule } from '@nestjs/jwt';
+import { LoginController } from './controllers/login.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    JwtModule.register({
+      secret: 'bucoenapbdoacnaocnsaoilchnsaoicsaoicnsaoiclnsaocnsancisbncoajcnsao',
+      signOptions: { expiresIn: '1h' },
+    })
+  ],
+  controllers: [AppController, LoginController],
+  providers: [AppService, LoginService, DatabaseService],
 })
-export class AppModule {}
+export class AppModule { }
