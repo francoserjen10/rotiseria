be9a40474dd1379f6f6aa3506204d6bd1ce604f3
Franco_Serjen <francoserjen100@gmail.com>
2024-06-04 20:06:42 -0300

    Modificaciones en el populado de la base de datos

--- db/script-populado.sql
+++ db/script-populado.sql
@@ -3,11 +3,11 @@ insert into roles (codigo, nombre) values ('ADM', 'Administrador');
 insert into roles (codigo, nombre) values ('USR', 'Usuario');
 
 -- Administradores
-insert into usuarios (nombre, apellido, dni, contrasenia, email, rolId) values ('Ruben', 'Torres', 46567989, '123456789', 'ruben.010.torres@gmail.com', 1);
-insert into usuarios (nombre, apellido, dni, contrasenia, email, rolId) values ('Franco', 'Serjen', 43738670, 'abcdef', 'francoserjen100@gmail.com', 1);
-insert into usuarios (nombre, apellido, dni, contrasenia, email, rolId) values ('Lucho', 'Rios', 43738677, '987654321', 'luchoRios@gmail.com', 1);
+insert into usuarios (nombre, apellido, dni, contrasenia, email, rolId) values ('administrador', 'administrador', 2, 'administrador', 'administrador@administrador.com', 1);
 
 -- selects
+select * from usuarios;
+select * from roles;
 select * from producto;
 select * from carrito;
 select * from carritoDetalle;
