91886c7c64c7342270c2eca137a1faed8cfda5bd
Franco_Serjen <francoserjen100@gmail.com>
2024-06-13 14:23:34 -0300

    Creacion de controlador y servicio de imagenes para almacenarlos en la plataforma imgbb y que los usuarios puedan guardar sus fotos

---
+++ src/common/controllers/images.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, HttpStatus, ParseFilePipeBuilder, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ImageService } from '../services/image.service';
+
+@Controller('/images')
+export class ImagesController {
+    constructor(private imageService: ImageService) {
+    }
+
+    @Post('/upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadImage(@Req() request,
+        @UploadedFile(
+            new ParseFilePipeBuilder()
+                .addFileTypeValidator({
+                    fileType: /(jpg|jpeg|png|gif)$/,
+                })
+                .addMaxSizeValidator({
+                    maxSize: 1024000,
+                    message: 'La imagen no puede superar el 1MB',
+                })
+                /*
+                Este error dice que el servidor entiende el tipo de contenido de la solicitud,
+                la sintaxis tambien es correcta,
+                pero no pudo procesar las instrucciones necesarias
+                */
+                .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY })
+        )
+        file: Express.Multer.File
+    ) {
+        const userData = JSON.parse(request.body.data);
+        return await this.imageService.upload(file, userData);
+    }
+}
---
+++ src/common/services/image.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import axiosInstance from 'src/axios/config';
+import { IUserDTO } from 'src/usuario/dto/usuario.dto';
+import { DatabaseService } from './db.service';
+import userQueries from 'src/usuario/queries/user.queries';
+const key: string = '6e9081ee31bf5d7ef88c15783c3b6bba';
+
+@Injectable()
+export class ImageService {
+
+    constructor(private dbService: DatabaseService) { }
+
+
+    async upload(file: Express.Multer.File, user: IUserDTO) {
+        try {
+            const formdata: FormData = new FormData();
+            //Buffer = arreglo de datos donde se guarda la imagen en base a numeros
+            formdata.append('image', new Blob([Buffer.from(file.buffer)]));
+            formdata.append('key', key);
+            formdata.append('name', 'nombre');
+
+            const response = await axiosInstance.post('/upload', formdata);
+
+            if (response && response.data) {
+                const responseData = response.data;
+
+                if (!responseData) {
+                    throw new Error("Ocurrio un error con responseData");
+                }
+
+                const imageUrl = response.data.data.url;
+                const deleteUrl = response.data.data.delete_url;
+                const displayUrl = response.data.data.display_url;
+
+                if (!imageUrl && !deleteUrl && !displayUrl) {
+                    throw new Error("Ocurrio un error con las url de las imagenes");
+                }
+
+                await this.dbService.executeSelect(userQueries.update, [imageUrl, deleteUrl, displayUrl, user.id])
+            }
+
+            return response.data;
+        } catch (error) {
+            console.error("Error al subir la imagen o actualizar la base de datos:", error);
+            throw new Error("Error al subir la imagen o actualizar la base de datos");
+        }
+    }
+}
