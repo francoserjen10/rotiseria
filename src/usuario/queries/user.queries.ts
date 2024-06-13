b56266f74998c001f996e831e00adf8cd7853258
Franco_Serjen <francoserjen100@gmail.com>
2024-06-13 19:38:10 -0300

    Arreglo de codigo que hacen referencia a propiedades de la db

--- src/usuario/queries/user.queries.ts
+++ src/usuario/queries/user.queries.ts
@@ -1,11 +1,11 @@
 const userQueries = {
-  insert: 'insert into usuarios (nombre, apellido, dni, contrasenia, email, rolId) values (?,?,?,?,?,?);',
-  selectAll: 'select * from usuarios',
+  insert: 'INSERT INTO usuarios (nombre, apellido, dni, contrasenia, email, rol_id) values (?,?,?,?,?,?);',
+  selectAll: 'SELECT * from usuarios',
   // selectById: 'select usuarioId, nombre, apellido, dni, contrasenia, email, rolId from usuarios where usuarioId = ?;',
-  selectByEmail: 'select * from usuarios where email = ?;',
+  selectByEmail: 'SELECT * from usuarios WHERE email = ?;',
   updateUser: 'UPDATE usuarios SET nombre = ?, apellido = ?, dni = ?, contrasenia = ?, email = ? WHERE usuario_id = ?;',
   updateImageUrl: 'UPDATE usuarios SET url_image = ?, url_image_delete = ?, display_url_image = ? WHERE usuario_id = ?;',
-  deleteById: 'delete from usuarios where usuarioId = ?;',
+  deleteById: 'DELETE from usuarios WHERE usuario_id = ?;',
 }
 
 export default userQueries;
