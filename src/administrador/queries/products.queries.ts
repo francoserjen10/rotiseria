a7f539cb661802d61f17050cdf8e98fb05fa27ea
Franco_Serjen <francoserjen100@gmail.com>
2024-06-18 17:20:33 -0300

    Creacion de controlador, servicio y querie de actualizacion de producto por id y a su vez se modifico el controlador de delete

--- src/administrador/queries/products.queries.ts
+++ src/administrador/queries/products.queries.ts
@@ -1,6 +1,7 @@
 const productQueries = {
     selectAll: 'SELECT * FROM producto',
     deleteById: 'DELETE FROM producto WHERE producto_id = ?',
+    updateById: 'UPDATE producto SET nombre = ?, descripcion = ?, precio = ?, rubro_id = ? WHERE producto_id = ?',
 }
 
 export default productQueries;
